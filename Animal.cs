a53182b748f4201258dd48daed2806885aaab207
onni82 <onni.bucht@me.com>
2024-10-18 16:43:43 +0200

    Created public abstract Animal()

---
+++ Animal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6_OOP_Arv
+{
+	public abstract class Animal
+	{
+		public string Skintype;
+		public string LocomotorOrgan;
+		public int Ears;
+		public string BreathingOrgan;
+		public bool HasTail;
+
+        public Animal(string skinType, string locomotorOrgan, int ears, string breathingOrgan, bool hasTail)
+        {
+			Skintype = skinType;
+			LocomotorOrgan = locomotorOrgan;
+			Ears = ears;
+			BreathingOrgan = breathingOrgan;
+			HasTail = hasTail;
+        }
+
+		public abstract void Eat();
+		public abstract void Sleep();
+		public abstract void Move();
+    }
+}
