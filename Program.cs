e67a6523ae20e109e7ff3179ffd876a18785f44f
onni82 <onni.bucht@me.com>
2024-10-20 16:25:47 +0200

    Removed Spectre.Console from project

--- Program.cs
+++ Program.cs
@@ -5,20 +5,7 @@ namespace Lab6_OOP_Arv
 	{
 		static void Main(string[] args)
 		{
-			// Ask for the user's favorite fruit
-			var fruit = AnsiConsole.Prompt(
-				new SelectionPrompt<string>()
-					.Title("What's your [green]favorite fruit[/]?")
-					.PageSize(10)
-					.MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-					.AddChoices(new[] {
-			"Apple", "Apricot", "Avocado",
-			"Banana", "Blackcurrant", "Blueberry",
-			"Cherry", "Cloudberry", "Cocunut",
-					}));
-
-			// Echo the fruit back to the terminal
-			AnsiConsole.WriteLine($"I agree. {fruit} is tasty!");
+			Console.WriteLine("");
 		}
 	}
 }
