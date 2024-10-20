a6df5912732a4d07962a40f8ac966b43e4af9a68
onni82 <onni.bucht@me.com>
2024-10-20 16:39:17 +0200

    Added animal objects in Main()

--- Program.cs
+++ Program.cs
@@ -5,7 +5,11 @@ namespace Lab6_OOP_Arv
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("");
+			Dog dog = new Dog("Dachshund", "fur", "legs", 2, "snout", true);
+			Chihuahua chihuahua = new Chihuahua();
+			Bulldog bulldog = new Bulldog();
+			Cat cat = new Cat();
+			Bird bird = new Bird();
 		}
 	}
 }
